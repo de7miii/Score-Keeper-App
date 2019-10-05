b2a701568fedb7d85e42354936253a6567cde81f
Abdelrahman Dafalla <daffallahabdulrahman@gmail.com>
2019-10-06 01:10:17 +0300

    Added a clock for the quarter time, and a text showing what is the current quarter.

--- app/src/main/java/com/example/basketballscoresapp/MainActivity.java
+++ app/src/main/java/com/example/basketballscoresapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.basketballscoresapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,32 +13,35 @@ public class MainActivity extends AppCompatActivity {
 
     private Team homeTeam;
     private Team awayTeam;
-//    private TextView qrtrTime;
+    private TextView qrtrTime;
 //    private TextView shotClk;
     private TextView homeScore;
     private TextView awayScore;
     private TextView homeFouls;
     private TextView awayFouls;
-//    private TextView firstQrtr;
-//    private TextView secondQrtr;
-//    private TextView thirdQrtr;
-//    private TextView fourthQrtr;
+    private TextView firstQrtr;
+    private TextView secondQrtr;
+    private TextView thirdQrtr;
+    private TextView fourthQrtr;
+
+    private CountDownTimer qrtrTimeTimer;
+    private int currentQrtr = 1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        qrtrTime = findViewById(R.id.quarter_time_text);
+        qrtrTime = findViewById(R.id.quarter_time_text);
 //        shotClk = findViewById(R.id.shot_clock_text);
         homeScore = findViewById(R.id.home_team_score);
         awayScore = findViewById(R.id.away_team_score);
         homeFouls = findViewById(R.id.home_team_fouls);
         awayFouls = findViewById(R.id.away_team_fouls);
-//        firstQrtr = findViewById(R.id.first_qrtr_text);
-//        secondQrtr = findViewById(R.id.second_qrtr_text);
-//        thirdQrtr = findViewById(R.id.third_qrtr_text);
-//        fourthQrtr = findViewById(R.id.fourth_qrtr_text);
+        firstQrtr = findViewById(R.id.first_qrtr_text);
+        secondQrtr = findViewById(R.id.second_qrtr_text);
+        thirdQrtr = findViewById(R.id.third_qrtr_text);
+        fourthQrtr = findViewById(R.id.fourth_qrtr_text);
         Button twoPointHomeBtn = findViewById(R.id.two_point_home_btn);
         Button threePointHomeBtn = findViewById(R.id.three_point_home_btn);
         Button freeThrowHomeBtn = findViewById(R.id.free_throw_home_btn);
@@ -46,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         Button foulAwayBtn = findViewById(R.id.foul_away_btn);
         Button resetBtn = findViewById(R.id.reset_btn);
 
-        initilizeData();
+        initializeData();
 
         twoPointHomeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,8 +75,7 @@ public class MainActivity extends AppCompatActivity {
         foulHomeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                homeTeam.commitFoul();
-                homeFouls.setText(Integer.toString(homeTeam.getFouls()));
+                updateHomeTeamFouls(1);
             }
         });
 
@@ -95,8 +100,7 @@ public class MainActivity extends AppCompatActivity {
         foulAwayBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                awayTeam.commitFoul();
-                awayFouls.setText(Integer.toString(awayTeam.getFouls()));
+                updateAwayTeamFouls(1);
             }
         });
         resetBtn.setOnClickListener(new View.OnClickListener() {
@@ -106,33 +110,132 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        updateQrtrText(currentQrtr);
+
+        // Change millisInFuture (the first argument in CountDownTimer() ) to 5000 (5s) for testing.
+        qrtrTimeTimer = new CountDownTimer(720000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int minutes = (int)millisUntilFinished/60000 ;
+                int seconds = (int)millisUntilFinished%60000/1000;
+                StringBuilder time = new StringBuilder();
+                time.append(minutes);
+                time.append(":");
+                if (seconds<10){
+                    time.append(0);
+                    time.append(seconds);
+                }else{
+                    time.append(seconds);
+                }
+                qrtrTime.setText(time);
+            }
+
+            @Override
+            public void onFinish() {
+                if (currentQrtr < 4){
+                    currentQrtr += 1;
+                    updateQrtrText(currentQrtr);
+                    start();
+                }else{
+                    reset();
+                }
+            }
+        }.start();
+
     }
 
-    private void initilizeData(){
+    public void updateQrtrText(int qrtr){
+        switch (qrtr){
+            case 1:
+                firstQrtr.setTextColor(Color.RED);
+                secondQrtr.setTextColor(Color.BLACK);
+                thirdQrtr.setTextColor(Color.BLACK);
+                fourthQrtr.setTextColor(Color.BLACK);
+                break;
+            case 2:
+                firstQrtr.setTextColor(Color.BLACK);
+                secondQrtr.setTextColor(Color.RED);
+                thirdQrtr.setTextColor(Color.BLACK);
+                fourthQrtr.setTextColor(Color.BLACK);
+                break;
+            case 3:
+                firstQrtr.setTextColor(Color.BLACK);
+                secondQrtr.setTextColor(Color.BLACK);
+                thirdQrtr.setTextColor(Color.RED);
+                fourthQrtr.setTextColor(Color.BLACK);
+                break;
+            case 4:
+                firstQrtr.setTextColor(Color.BLACK);
+                secondQrtr.setTextColor(Color.BLACK);
+                thirdQrtr.setTextColor(Color.BLACK);
+                fourthQrtr.setTextColor(Color.RED);
+                break;
+            default:
+                firstQrtr.setTextColor(Color.BLACK);
+                secondQrtr.setTextColor(Color.BLACK);
+                thirdQrtr.setTextColor(Color.BLACK);
+                fourthQrtr.setTextColor(Color.BLACK);
+                break;
+        }
+    }
+
+
+    private void initializeData(){
         homeTeam = new Team("Home");
         awayTeam = new Team("Away");
     }
 
     private void updateHomeTeamScore(int points){
-        // TODO: 10/3/2019 update the home team score according to the button clicked
-        homeTeam.scorePoints(points);
-        homeScore.setText(Integer.toString(homeTeam.getScore()));
+        if (points == 0){
+            homeTeam.setScore(0);
+        }else {
+            homeTeam.scorePoints(points);
+        }
+        StringBuilder score = new StringBuilder();
+        score.append(homeTeam.getScore());
+        homeScore.setText(score);
     }
 
     private void updateAwayTeamScore(int points){
-        // TODO: 10/3/2019 update the away team score according to the button clicked
-        awayTeam.scorePoints(points);
-        awayScore.setText(Integer.toString(awayTeam.getScore()));
+        if (points == 0){
+            awayTeam.setScore(0);
+        }else {
+            awayTeam.scorePoints(points);
+        }
+        StringBuilder score = new StringBuilder();
+        score.append(awayTeam.getScore());
+        awayScore.setText(score);
+    }
+
+    private void updateHomeTeamFouls(int foulPoint){
+        if (foulPoint == 0){
+            homeTeam.setFouls(0);
+        }else{
+            homeTeam.commitFoul();
+        }
+        StringBuilder foulsString = new StringBuilder();
+        foulsString.append(homeTeam.getFouls());
+        homeFouls.setText(foulsString);
+    }
+
+    private void updateAwayTeamFouls(int foulPoint){
+        if (foulPoint == 0){
+            awayTeam.setFouls(0);
+        }else{
+            awayTeam.commitFoul();
+        }
+        StringBuilder foulsString = new StringBuilder();
+        foulsString.append(awayTeam.getFouls());
+        awayFouls.setText(foulsString);
     }
     private void reset(){
-        // TODO: 10/3/2019 reset the game score, fouls, and clock
-        homeTeam.setScore(0);
-        homeScore.setText(Integer.toString(homeTeam.getScore()));
-        homeTeam.setFouls(0);
-        homeFouls.setText(Integer.toString(homeTeam.getFouls()));
-        awayTeam.setScore(0);
-        awayScore.setText(Integer.toString(awayTeam.getScore()));
-        awayTeam.setFouls(0);
-        awayFouls.setText(Integer.toString(awayTeam.getFouls()));
+        updateHomeTeamScore(0);
+        updateHomeTeamFouls(0);
+        updateAwayTeamScore(0);
+        updateAwayTeamFouls(0);
+        currentQrtr = 1;
+        updateQrtrText(currentQrtr);
+        qrtrTimeTimer.onTick(720000);
+        qrtrTimeTimer.start();
     }
 }
